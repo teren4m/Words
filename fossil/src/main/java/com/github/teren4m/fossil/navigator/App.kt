f5a14102063203105f950268e2e48729ed789232
Tim Andriyaschenko <timAndriyaschenko@gmail.com>
2019-04-20 09:40:21 +0300

    Add request for permission

---
+++ fossil/src/main/java/com/github/teren4m/fossil/navigator/App.kt
@@ -0,0 +1,12 @@
+package com.github.teren4m.fossil.navigator
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
